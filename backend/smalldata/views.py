b01406e742cbb83129d904a5789891fcd7b1f2ef
staude <stoide@googlemail.com>
2019-06-17 17:08:19 +0200

    API for utterance model

--- backend/smalldata/views.py
+++ backend/smalldata/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UtteranceSerializer
+from .models import Utterance
 
-# Create your views here.
+
+class UtteranceView(viewsets.ModelViewSet):
+    serializer_class = UtteranceSerializer
+    queryset = Utterance.objects.all()
