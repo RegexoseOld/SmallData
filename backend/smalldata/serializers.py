13700c5fc7d75d9ab4a923a9248ac588654ee0ab
staude <stoide@googlemail.com>
2019-06-18 21:26:40 +0200

    Make Category a nullable ForeignKey

--- backend/smalldata/serializers.py
+++ backend/smalldata/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Utterance, Category
 
 
-class UtteranceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Utterance
-        fields = ('id', 'text')
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'description')
+
+
+class UtteranceSerializer(serializers.ModelSerializer):
+    category_id = CategorySerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Utterance
+        fields = '__all__'
