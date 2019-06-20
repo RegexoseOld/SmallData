e3f0a791a03b35849f88713b4b9756d2dbbea2ca
borisjoens <joens@snafu.de>
2019-06-20 21:52:08 +0200

    renamed class Edit to Manipulate Created Manipulate.py moved class Manipulate edited stop() function

--- src/MusicServer03.py
+++ src/MusicServer03.py
@@ -6,14 +6,13 @@ import logging
 import pickle
 import collections
 
-import copy
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
 from MusicObjects import Noten, Chords
 from PlayObjects import Play, To_Client_Thread, Play_Thread, play_dict, slotlist, playmap, PLAYTHREADS
+from Manipulate import Manipulate
 from UDPClient import Client_MusicServer, START
-from rules import normal_values
 
 
 logging.basicConfig(level = logging.DEBUG, format = '(%(threadName)-10s) %(message)s',)
@@ -32,28 +31,7 @@ TRIGGERMAP = {
     'end': [i for i in range(23,26)]
 
 }
-RULES = {
-    'half': 0.5,
-    'default':37,
-    '=': 0,
-    '+1': 1,
-    '+2': 2,
-    '+3': 3,
-    '+4': 4,
-    '+6': 6,
-    '+10': 10,
-    '+11': 11,
-    'double':2,
-    '4x': 4,
-    'viertel': 0.25,
-    '-1': -1,
-    '-2': -2,
-    '-4':-4,
-    '-5': -5,
-    '-7': -7,
-    'slower': -2,
-    'faster': 2
-}
+
 
 def dict_values(tracks, fader):
     addresses = []
@@ -66,138 +44,11 @@ def dict_values(tracks, fader):
 
 LIVE_VALUES = dict_values(TRACKS, FADER)
 
-# print('LIVE_VALUES 48: ', LIVE_VALUES)
-
-
-class Edit:
-    def __init__(self, Noten_objekt, Play_object, client):
-        self.N_Obj = Noten_objekt
-        self.P_Obj = Play_object
-        self.Copy_Obj = copy.deepcopy(self.N_Obj)
-        self.playmap = {}
-        self.user = self.N_Obj.name
-        self.userlist = []
-        self.replist = list(self.playmap.values())
-        self.client = client
-        self.songposition = 0
-        self.point = ""
-        self.user_count = 1
-
-    def __repr__(self):
-        return '(edit {})'.format(self.Copy_Obj)
-
-    def set_playmap(self, playmap):
-        self.playmap = playmap
-
-    def function_dispatch(self, user, address, client_map):
-
-        if address == '/controlmap':
-            print('\tcontrolmap')
-            self.update_controllers(client_map)
-            #print('new values', self.N_Obj.vel, self.P_Obj.wheel, self.P_Obj.ccnr,  self.P_Obj.ccval)
-
-        elif address == '/start':
-            self.new_Thread(user, client_map)
-
-    def update_controllers(self, cl_map):
-        print('cl_map ', cl_map)
-
-        coarse = cl_map['coarse']
-        trigger = cl_map['trigger']
-        wheel = cl_map['wheel']
-        cc_map = cl_map['cc_dict']
-        threadPlay = PLAYTHREADS['t1'].P_Obj
-        playthread = PLAYTHREADS['t1']
-        playthread.play = True
-
-        if cc_map == 'normal_values':
-            # print('normvals: ', normal_values)
-            PLAYTHREADS['t1'].send_ccvals(normal_values)
-            threadPlay.cc_map = normal_values
-        else:
-            print('else: ', threadPlay.cc_map == normal_values)
-            playthread.send_ccvals(cc_map)
-            threadPlay.cc_map = cc_map
-
-        threadPlay.wheel = wheel
-        if coarse in ['half', 'double', 'viertel', '4x']:
-            threadPlay.coarse *= RULES[coarse]
-        elif coarse in ['slower', 'faster']:
-            threadPlay.coarse += RULES[coarse]
-        elif coarse == 'default':
-            threadPlay.coarse = RULES[coarse]
-
-        ruletrigger = RULES[trigger]
-        position = playthread.slot
-        self.songposition = copy.deepcopy(position)
-        print('aktuelle position {} self.songposition: {} ruletrigger: {}'.format(position, self.songposition, ruletrigger))
-
-        if (position + RULES[trigger]) >= 25:
-            print('position  {} + RULES[trigger]: {} = {}'.format(position, RULES[trigger], (position + RULES[trigger])))
-            playthread.slot = 25
-            playthread.jump(25)
-            playthread.send_ccvals(normal_values)
-
-        elif trigger in [ "+1", "-1", "+2", "-2", '-5', '-7', '+3', '+4', '+6','+10', '+11']:
-            slot = playthread.slot + RULES[trigger]
-            playthread.play = False
-            playthread.jump(slot)
-            playthread.slot = slot
-            playthread.send_ccvals(normal_values)
-            playthread.cond_set(slot)
-            print('genullt?: ', playthread.ccmap == normal_values)
-
-        elif trigger == '=':
-            playthread.slot += RULES[trigger]
-
-        elif trigger in ["-1", "-2"] and position == 0:
-            playthread.slot = 0
-
-        self.P_Obj = threadPlay
-
-    def new_Thread(self, user, cl_map):
-        noteIds = list(cl_map.keys())
-        values = list(cl_map.values())
-        notes = [i[0] for i in values]
-        vel = [i[1] for i in values]
-        ccnr = [i[3] for i in values]
-        ccval = [i[4] for i in values]
-        #print('notes 157: ', notes)
-        dummy = [0 for i in range(len(values))]
-        tm = [i[2] for i in values]
-        if user in self.userlist:
-            print('User exists!! ')
-            self.playmap = generate_playmap([1, 1, 1, 1, 2, 2], [0, 1, 2, 3, 4, 5])
-            t1 = PLAYTHREADS['t1']
-            t1.play = False  # bisheriger Playthread wird angehalten
-            t2 = Play_Thread(user, self.playmap, self.client, COND2)
-            t2.start()
-            t1.join()  # t1 wird beendet
-            print('Playmap 153: ', t2.playmap)
-            print('thread: {} t1.play {}, alive? {} '.format(t1.name, t1.play,
-                                                             t1.is_alive()))  # Abfrage ob t1 auch wirklich tot ist
-            del PLAYTHREADS['t1']
-            PLAYTHREADS['t1'] = t2
-
-        else:
-            self.userlist.append(user)
-            self.playmap = generate_playmap([1, 1, 1, 1, 2, 2], [0, 1, 2, 3, 4, 5])
-            t1 = PLAYTHREADS['t1']
-            t1.play = False  # bisheriger Playthread wird angehalten
-            t2 = Play_Thread(user, self.playmap, self.client, COND2)
-            t2.start()
-            t1.join()
-            # print('thread: {} t1.play {}, alive? {} t2.play {} '.format(t1.name, t1.play,
-            #                                                             t1.is_alive(), t2.play))
-            PLAYTHREADS.pop('t1')
-            PLAYTHREADS['t1'] = t2
-            print('Playthreads 255: ', PLAYTHREADS)
-
 class Live:
-    def __init__(self, client, edit, message, cond):
+    def __init__(self, client, manipulate, message, cond):
         self.client = client
         self.cond = cond
-        self.edit = edit
+        self.manipulate = manipulate
         self.message = message
         self.position = 108
         self.templist = [time.time()]
@@ -223,14 +74,14 @@ class Live:
             del cl_map['point']
             with self.client.cond:
                 self.client.cond.wait() # wartet auf den taktstart der eigenen COND1 ..?
-                self.edit.function_dispatch(user, address, point, cl_map)
+                self.manipulate.function_dispatch(user, address, point, cl_map)
                 print('\tmelo edit: taktstart: ', self.position)
 
         elif address == '/buildmap':
             user = cl_map['user']
             del cl_map['user']
             # print('\n\tclientmap: {}, user: {}, address: {}\n'.format(cl_map, username, address))
-            self.edit.function_dispatch(user, address, 'akk_01', cl_map)
+            self.manipulate.function_dispatch(user, address, 'akk_01', cl_map)
             # with self.client.cond:
             #     self.client.cond.wait()
             #     print('\tnew notes taktstart: ', self.beat)
@@ -241,7 +92,7 @@ class Live:
             repeat = cl_map['repeat']
             del cl_map['user']
             del cl_map['repeat']
-            self.edit.function_dispatch(user, address, cl_map)
+            self.manipulate.function_dispatch(user, address, cl_map)
             PLAYTHREADS['t1'].playmap_check(repeat)
 
     def message_handler(self, address, msg):
@@ -258,7 +109,7 @@ class Live:
         elif address == '/controlmap':
                 user = cl_map['user']
                 del cl_map['user']
-                self.edit.function_dispatch(user, address, cl_map)
+                self.manipulate.function_dispatch(user, address, cl_map)
         #
         # elif isinstance(cl_map[''], float) and len(cl_map) >= 3:
         #     live_value = LIVE_VALUES[cl_map[0]]
@@ -276,7 +127,7 @@ class Live:
         #         LIVE_VALUES[cl_map[0]] = round(live_value, 3) + round(cl_map[1], 3)
 
         elif cl_map[0] == '/start':
-            self.edit.function_dispatch(cl_map[1], cl_map[0],  {'dummy': 'dummy1'} )
+            self.manipulate.function_dispatch(cl_map[1], cl_map[0], {'dummy': 'dummy1'})
 
         else:
             LIVE_VALUES[cl_map[0]] = cl_map[1]
@@ -313,8 +164,8 @@ class Live:
             self.templist.append(time.time())
             #print('position: {}  tempo: {}'.format(self.position, self.tempo))
             self.tempo = self.tiempo(self.templist)
-            self.edit.P_Obj = PLAYTHREADS['t1'].P_Obj
-            self.edit.P_Obj.set_tempo(self.tempo * 4)
+            self.manipulate.P_Obj = PLAYTHREADS['t1'].P_Obj
+            self.manipulate.P_Obj.set_tempo(self.tempo * 4)
 
             if self.position == 115:
                 #self.pre += 1
@@ -358,7 +209,7 @@ class Live:
 
 def mainloop(server, osculator, playmap):
     print('\n \t !!!!!!!!\n')
-    edit.set_playmap(playmap)
+    manipulate.set_playmap(playmap)
     t1 = Play_Thread('play_init', playmap, play_init, osculator, COND2, COND3)
     tcl = To_Client_Thread('for_interface', server_instance_client, COND3)
     PLAYTHREADS['t1'] = t1
@@ -405,8 +256,8 @@ if __name__ == "__main__":
 
     play_init = play_dict['slot1']
 
-    edit = Edit(chords_0, play_init, osculator_instance)
-    live = Live(osculator_instance, edit, ["/trig_master", 1], COND2)
+    manipulate = Manipulate(chords_0, play_init, COND2, osculator_instance)
+    live = Live(osculator_instance, manipulate, ["/trig_master", 1], COND2)
 
     '''muss ganz oben stehen, sonst versteht der server die dispatcher adressen nicht'''
 
@@ -435,6 +286,5 @@ if __name__ == "__main__":
         mainloop(server, osculator_instance, playmap)
 
     except KeyboardInterrupt:
-        pass
-        # stop()
+        stop()
 
