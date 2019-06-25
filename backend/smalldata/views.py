38895f8bcf73b3642ed5d98c1fad889413485c14
staude <stoide@googlemail.com>
2019-06-25 10:40:23 +0200

    Improve placeholder for classification inclusion

--- backend/smalldata/views.py
+++ backend/smalldata/views.py
@@ -9,15 +9,18 @@ class UtteranceView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         #  TODO: First detect the correct category,
-        serializer.validated_data["category"] = self.__get_category_for_utterance(serializer)
+        #Fetch sent data:
+        text = serializer.validated_data["text"]
+
+        """
+        #predict class
+        category_name, prob = Classifier.predict_proba(text)
+        """
+        category_name = 'Lob'
+        category = Category.objects.all().filter(name=category_name)[0]
+        serializer.validated_data["category"] = category
         #  TODO:  Second, send the category to the music server
         super(UtteranceView, self).perform_create(serializer)
-
-    def __get_category_for_utterance(self, serializer):
-        category = None
-        if len(Category.objects.all())>0:
-            category = Category.objects.all()[0]
-        return category
 
 
 class CategoryView(viewsets.ModelViewSet):
