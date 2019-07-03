c6e2a2861bcd5bc6d248b27056246add3aeada2b
borisjoens <joens@snafu.de>
2019-07-03 19:58:15 +0200

    not quiet working With new utterance entered into form, this ERROR happens
    ValueError at /api/utterances/
    Cannot assign "<QuerySet [<Category: Category object (1)>]>": "Utterance.category" must be a "Category" instance.

--- backend/smalldata/views.py
+++ backend/smalldata/views.py
@@ -1,8 +1,10 @@
+import sys
+
 from rest_framework import viewsets
 from .serializers import UtteranceSerializer, CategorySerializer
 from .models import Utterance, Category
 sys.path.insert(0,'/Users/borisjoens/Dropbox/Kommentare/SmallData/src')
-from Classifier_max import Classifier
+# from Classifier_max import Classifier
 
 # print('sys path;', sys.path[0])
 
@@ -19,8 +21,10 @@ class UtteranceView(viewsets.ModelViewSet):
         #predict class
         category_name, prob = Classifier.predict_proba(text)
         """
+        # cat, prob = Classifier.predict_proba(text, verbose=True)
         category_name = 'Lob'
-        category = Category.objects.all().filter(name=category_name)[0]
+
+        category = Category.objects.all().filter(name=category_name)
         serializer.validated_data["category"] = category
         #  TODO:  Second, send the category to the music server
         super(UtteranceView, self).perform_create(serializer)
