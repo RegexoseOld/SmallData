5d89c724c14082fe982a5577cb0749e8176b0bee
staude <stoide@googlemail.com>
2019-09-29 16:53:24 +0200

    Add display logic in SongServer

--- song/song_server.py
+++ song/song_server.py
@@ -1,51 +1,94 @@
+from pythonosc.osc_server import AsyncIOOSCUDPServer
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import udp_client
+
 import pickle
-import json
-from pythonosc import dispatcher, osc_server, udp_client
+import asyncio
+import pygame
+
 
 INTERPRETER_TARGET_ADDRESS = "/interpreter_input"
+INTERPRETER_PORT = 5020
+OSCULATOR_PORT = 5010
+
+pygame.init()
+pygame.font.init()
+font = pygame.font.Font(None, 20)
+
+ip = "127.0.0.1"
+
+
+size = width, height = 420, 240
+black = 0, 0, 0
+font_color = 155, 155, 0
+refresh_rate = 10.  # Hz
 
 
 class SongServer:
-    """
-    Receives msseages from the Interpreter translates them and forwards the result to the osculator
-    """
-    def __init__(self, client, song):
-        self.osculator_client = client
-        self._song_machine = song
-        song_dispatcher = dispatcher.Dispatcher()
-        song_dispatcher.map(INTERPRETER_TARGET_ADDRESS, lambda address, content: self.message_handler(address, content))
-        self.interpreter_server = osc_server.ThreadingOSCUDPServer(('127.0.0.1', 5020), song_dispatcher)
+    server = None
 
-    def message_handler(self, address, osc_map):
-        osc_map = pickle.loads(osc_map)
-        print('address: {} map: {}'.format(address, osc_map))
+    def __init__(self, client, machine):
+        self.osculator_client = client
+        self._song_machine = machine
+
+        self.screen = pygame.display.set_mode(size)
+        self.interpreter_output_surf = font.render('Nothing received', False, font_color)
+        self.song_state_surf = font.render('Nothing received', False, font_color)
+
+    def _update_display_objects(self, osc_map):
+        self.interpreter_output_surf = font.render('Received map: {}'.format(osc_map), True, font_color)
+        self.song_state_surf = font.render('Current Part {}'. format(self._song_machine.current_state.name),
+                                           True, font_color)
+
+    def _update_song(self, osc_map):
         level = osc_map['level']
+        self.osculator_client.send_message('/rack', (level / 10))
+
         current_state = self._song_machine.current_state
         self._song_machine.update_state(osc_map['cat'])
         if current_state != self._song_machine.current_state:
-            self.osculator_client.send_message('/rack', (level / 10))
             self.osculator_client.send_message('/advance', (0, 1.0))  # was braucht Osculator hier?
             self.osculator_client.send_message('/advance', (0, 0.0))
 
-    def serve_forever(self, *args):
-        self.interpreter_server.serve_forever(*args)
+    def message_handler(self, address, content):
+        osc_map = pickle.loads(content)
+        print('address: {} map: {}'.format(address, osc_map))
+        self._update_song(osc_map)
+        self._update_display_objects(osc_map)
+
+    async def loop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+
+            self.screen.fill(black)
+            self.screen.blit(self.interpreter_output_surf, (0, 0))
+            self.screen.blit(self.song_state_surf, (0, 30))
+            pygame.display.flip()
+
+            await asyncio.sleep(1./refresh_rate)
+
+    async def init_main(self):
+        dispatcher = Dispatcher()
+        dispatcher.map(INTERPRETER_TARGET_ADDRESS, self.message_handler)
+
+        self.server = AsyncIOOSCUDPServer((ip, INTERPRETER_PORT), dispatcher, asyncio.get_event_loop())
+        transport, protocol = await self.server.create_serve_endpoint()  # Create datagram endpoint and start serving
+
+        await self.loop()  # Enter main loop of program
+
+        transport.close()  # Clean up serve endpoint
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    mock_osculator_client = udp_client.SimpleUDPClient(ip, OSCULATOR_PORT)
+
     # add file to path so import works, see https://stackoverflow.com/a/19190695/7414040
     from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
     from song import song_machine
 
-    mock_osculator_client = udp_client.SimpleUDPClient('127.0.0.1', 5010)
-    path_to_song_file = '../config/heavy_lemon.json'
-    with open(path_to_song_file, 'r') as f:
-        json_data = json.load(f)
+    machine = song_machine.create_instance()
 
-    song_machine.SongValidator(json_data).validate()
-    json_parser = song_machine.SongParser(json_data)
-    json_parser.parse()
-    song_machine = song_machine.SongMachine(json_parser)
-
-    song_server = SongServer(mock_osculator_client, song_machine)
-    song_server.serve_forever()
+    song_server = SongServer(mock_osculator_client, machine)
+    asyncio.run(song_server.init_main())
