2af8822a37dcd825779c6c334899d49491f5d40a
Broris <boris.joens@gmail.com>
2019-10-01 17:47:14 +0200

    added more texts to Mock_Interpreter_Client.
    _position:text_display needs more work

--- mocks/Mock_Interpreter_Client.py
+++ mocks/Mock_Interpreter_Client.py
@@ -4,6 +4,11 @@ import random
 import pickle
 
 mock_client = SimpleUDPClient('127.0.0.1', 5020)
+texts = ['Dies ist der nullte Kommentar von Mock_Interpreter_Client',
+         'Dies ist der erste Kommentar',
+         'der zweite Kommentar' ,
+         'der dritte Kommentar von Mock_Interpreter_Client',
+         'Dies ist der vierte und finite Kommentar von Mock_Interpreter_Client']
 categories = ['Kritik', 'Lob']
 level_values = [3, 5, 8]
 
@@ -17,9 +22,9 @@ if __name__ == "__main__":
     from song import song_server
 
     while True:
-        osc_dict = {'text' : 'Dies ist ein Kommentar von Mock_Interpreter_Client',
+        osc_dict = {'text' : texts[random.randint(0, 4)],
                     'cat': categories[random.randint(0, 1)],
                     'level': level_values[random.randint(0, 2)]}
         osc_map = pickle.dumps(osc_dict)
         mock_client.send_message(song_server.INTERPRETER_TARGET_ADDRESS, osc_map)
-        time.sleep(5)
+        time.sleep(2)
--- song/song_server.py
+++ song/song_server.py
@@ -3,6 +3,7 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import udp_client
+from collections import OrderedDict
 
 import pickle
 import asyncio
@@ -19,7 +20,7 @@ font = pygame.font.Font(None, 20)
 ip = "127.0.0.1"
 
 
-size = width, height = 800, 450
+size = width, height = 800, 800
 black = 0, 0, 0
 grey = 227, 227, 227
 font_color = 155, 155, 0
@@ -36,14 +37,36 @@ class SongServer:
 
         self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption('Status Screen')
+        self.text_surface = pygame.Surface((width, height/2))
+        self.text_surface.fill(grey)
 
-        self.interpreter_output_surf = font.render('Nothing received', False, font_color)
-        self.song_state_surf = font.render('Nothing received', False, font_color)
+        self.interpreter_output_surf = font.render('No Interpretation Received', False, font_color)
+        self.text_positions = OrderedDict()
+        self.pos_y = 0
+        self.song_state_surf = font.render('No State Received', False, font_color)
+        self.text_surface.blit(self.interpreter_output_surf, (0, 0))
+
+    def _position_text_display(self, text, text_cache=3):
+        self.text_positions[text] = text.get_rect().bottom + self.pos_y
+        if len(self.text_positions) == text_cache + 1:
+            # replace position of text to the position of the frontrunner
+            # and delete first key 
+            for text, position in self.text_positions.items():
+                for index, item in enumerate(:
+                if index != 0:
+                pass
+
+
+        self.pos_y = text.get_rect().bottom + self.pos_y
+        self.text_surface.blit(text, (0, self.pos_y))
+
 
     def _update_display_objects(self, osc_map):
+        # self.text_surface.fill(grey)
         self.interpreter_output_surf = font.render('Received map: {}'.format(osc_map), True, font_color)
         self.song_state_surf = font.render('Current Part {}'. format(self._song_machine.current_state.name),
                                            True, font_color)
+        self._position_text_display(self.interpreter_output_surf)
         self.song_graphic.playhead.handle_input_data(self._song_machine.current_state.name)
 
     def _update_song(self, osc_map):
@@ -68,12 +91,14 @@ class SongServer:
                 if event.type == pygame.QUIT: sys.exit()
 
             self.screen.fill(grey)
-            self.screen.blit(self.interpreter_output_surf, (15, 350))
-            self.screen.blit(self.song_state_surf, (15, 380))
+
+            self.screen.blit(self.song_state_surf, (15, 325))
+            self.screen.blit(self.text_surface, (15, 350))
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     self.song_graphic.handle_input(event.key)
+
             self.song_graphic.playhead.handle_input_data(1) # moves playhead forward
             self.song_graphic.render(self.screen)
             pygame.display.flip()
