180ed10bf6a3a35d76580a781644f62f4dc8f73e
Broris <boris.joens@gmail.com>
2019-10-14 22:46:38 +0200

    created font_render for line breaks

---
+++ display/font_render.py
@@ -0,0 +1,46 @@
+from os import sys, path
+# sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+import pygame
+
+def linebreak(text, color, rect, font, aa=False, bkg=None):
+    # aa = antialiased bkg= background color
+    # print('text: {}\ncolor  {}\nrect: {}'.format(text, color, rect))
+    surface = pygame.Surface((rect.width, rect.height))
+    surface.fill((50, 74, 168))
+    y = rect.top
+    line_spacing = -2
+
+    #get height of font
+    fontHeight = font.size("Tg")[1]
+
+    while text:
+        i = 1 # index of characters
+        # determine if row of text will be outside area
+        if y + fontHeight > rect.bottom:
+            break
+        # determine max width of line
+        while font.size(text[:i])[0] < rect.width and i < len(text) +1:
+            #print('font.size(text[:i])[0]:   ', font.size(text[:i])[0])
+            #print('text[:i]', text[:i])
+            #print('i', i)
+            i += 1
+
+        # if text is wrapped then adjust the wrap to the last word
+        if i < len(text):
+            #print('i: {} len(text): {}'.format(i, len(text)))
+            i = text.rfind(" ", 0, i)
+
+        # render the line and blit on surface
+        if bkg:
+            image = font.render(text[:i], 1, color, bkg)
+            image.set_colorkey(bkg)
+        else:
+            image = font.render('{}'.format(text[:i]), aa, color)
+        y += fontHeight + line_spacing
+
+        surface.blit(image, (rect.left, y))
+
+        #remove text (line) we just blitted
+        text = text[i:] # the remaining text
+
+    return surface
