e7423be9de1ddb6426f1ae1c6da05e190a1fe641
staude <stoide@googlemail.com>
2019-11-17 20:40:48 +0100

    Use state to represent form data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,28 +7,36 @@ import fetch from "node-fetch";
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            text: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        let userData =  this.input.value;
         fetch("http://localhost:8000/api/utterances/", {
             method: "POST",
-            body: JSON.stringify({text: userData}),
+            body: JSON.stringify(this.state),
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
         }).then(response => {
             (response.json().then(data => {alert('You said: ' + data['text'] +
-                '.\n Machine thinks: ' + data['category']['name'])
+                '\n Machine thinks: ' + data['category']['name'])
             }).then(val => this.resetForm()))
         });
     }
 
+    handleChange(event) {
+        this.setState({text: event.target.value});
+    }
+
     resetForm() {
-        this.input.value = "";
+        this.setState({text: ''});
     }
 
     render() {
@@ -44,19 +52,20 @@ class App extends Component {
 
                 <div className="row ">
                 <form onSubmit={this.handleSubmit}>
-                    <textarea type="text" style={{width: 350}} ref={(input) => this.input = input} />
-                </form>
-                </div>
-
-                <div className="row ">
+                    <textarea type="text" style={{width: 350}}
+                              onChange={this.handleChange}
+                              value={this.state.text}
+                    />
+                    <div>
                     <input type="submit" value="Submit" />
+                    </div>
+                </form>
                 </div>
 
                 </div>
             </main>
         );
     }
-
 
 }
 
