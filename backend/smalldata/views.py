3a6cf14db5dfb5d6c7d96723497cd21049053abe
staude <stoide@googlemail.com>
2019-11-18 11:46:27 +0100

    Send utterance and category per OSC

--- backend/smalldata/views.py
+++ backend/smalldata/views.py
@@ -1,5 +1,6 @@
 import time
 import os
+import pickle
 
 from backend.settings import BASE_DIR
 from rest_framework import viewsets
@@ -7,13 +8,13 @@ from .serializers import UtteranceSerializer, CategorySerializer, TrainingUttera
 from .models import Utterance, Category, TrainingUtterance
 
 from classification.Classifier_max import Classifier
-from sound.UDPClient import Client_MusicServer
+from sound.UDPClient import MusicClient, INTERPRETER_PORT, INTERPRETER_TARGET_ADDRESS
 from sound.rules import SIMPLE_NOTES
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 clf = Classifier(os.path.join(BASE_DIR, 'model_data'))
 #   Client for a simple Feedback from Ableton Live
-music_client = Client_MusicServer('127.0.0.1', 5015)
+music_client = MusicClient('127.0.0.1', INTERPRETER_PORT)
 
 
 class UtteranceView(viewsets.ModelViewSet):
@@ -21,36 +22,32 @@ class UtteranceView(viewsets.ModelViewSet):
     queryset = Utterance.objects.all()
 
     def perform_create(self, serializer):
-        #  TODO: First detect the correct category,
+        #  First detect the correct category,
         # Fetch sent data:
         print(serializer.validated_data)
         text = serializer.validated_data["text"]
         # send text to clf to return a category
         cat, prob = clf.predict_proba(text, verbose=True)
         print('cat: {}\nproba {}'.format(cat, prob))
-        # lookup found category in database
 
+        # lookup found category in database
         #  TODO: build a test during startup to make sure the db and the model reproduce the same categories!
         categories = Category.objects.all().filter(name=str(cat))
         if not categories:  # if db is inconsistent with model, just use any cat
             categories = Category.objects.all()
             print('WARNING: no matching category in db! Using random assignment')
         category = categories[0]
-
         serializer.validated_data["category"] = category
-        #  Second, send the category to the music server
-        #  TODO: build a test during startup to make sure the db and the model reproduce the same categories!
-        if cat not in SIMPLE_NOTES:
-            note = SIMPLE_NOTES.popitem()[1]
-            print('WARNING: category not contained in SIMPLE_NOTES! Using random assignment')
-        else:
-            note = SIMPLE_NOTES[cat]
-        print('note: ', note)
-        
-        music_client.msg_send(note, 100, 1.0)
-        time.sleep(1)
-        music_client.msg_send(note, 100, 0.0)
-        # display found category in the app
+
+        #  Send the category to the music server
+        osc_dict = {
+            'text': text,
+            'cat': category
+        }
+        osc_map = pickle.dumps(osc_dict)
+        music_client.send_message(INTERPRETER_TARGET_ADDRESS, osc_map)
+
+        #  save result in db
         super(UtteranceView, self).perform_create(serializer)
 
 
