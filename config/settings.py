d856d74efc57454ea6f1a1d868f58f7ae49a3bf4
staude <stoide@googlemail.com>
2019-12-09 16:19:53 +0100

    move osc-adresses to settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,3 +7,13 @@ DATA_DIR = path.join(BASE_DIR, 'webserver/model_data')
 # Song settings
 song_file = 'praise.json'
 song_path = path.join(BASE_DIR, 'config', song_file)
+
+# OSC-Settings
+INTERPRETER_TARGET_ADDRESS = "/interpreter_input"
+INTERPRETER_PORT = 5020
+OSCULATOR_PORT = 5010
+
+DISPLAY_TARGET_ADDRESS = "/display_input"
+DISPLAY_PORT = 5030
+
+ip = "127.0.0.1"
