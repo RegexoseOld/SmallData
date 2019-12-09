6feb95a08cbef2c8952532f2c20b748cbe65bf80
staude <stoide@googlemail.com>
2019-12-09 16:20:38 +0100

    include ‘display’ and ‘mock’ in run.py

--- run.py
+++ run.py
@@ -37,9 +37,41 @@ if not args.skip_checks:
 if args.app == 'backend':
     p = subprocess.check_call(["python", "webserver/manage.py", "runserver"])
 elif args.app == 'song':
-    p = subprocess.check_call(["python", "song/song_server.py"])
+    from pythonosc import udp_client
+    from os import sys, path
+
+    # sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    from song import song_machine
+    from song.song_server import SongServer
+
+    oscul_client = udp_client.SimpleUDPClient(settings.ip, settings.OSCULATOR_PORT)
+    disp_client = udp_client.SimpleUDPClient(settings.ip, settings.DISPLAY_PORT)
+
+    machine_instance = song_machine.create_instance(settings.song_path)
+
+    song_server = SongServer(oscul_client, disp_client, machine_instance)
+    song_server.server.serve_forever()
+    # p = subprocess.check_call(["python", "song/song_server.py"])
 elif args.app == 'frontend':
     os.chdir('frontend')
     p = subprocess.check_call(["npm", "start"])
+elif args.app == 'display':
+    from song.display.playhead import Playhead
+    from song.display.status_display import SongStatus
+    from song.display.display_server import DisplayServer
+    import asyncio
+
+    playhead = Playhead()
+    #  TODO use actula states of song
+    song_graphic = SongStatus(settings.song_file, ["intro", "scene02", "scene03"], playhead)
+    display_server = DisplayServer(song_graphic)
+
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(display_server.init_main())
+elif args.app == 'mock':
+    from mocks import Mock_Interpreter_Client
+
+    Mock_Interpreter_Client.run_mock()
+
 else:
     raise Exception('Unknown command: {}'.format(args.app))
