18b27538d320de75e8a960ddb9a35fa9475b8a35
staude <stoide@googlemail.com>
2019-12-09 16:21:03 +0100

    Improve logic of Mock_Interpreter_Client

--- mocks/Mock_Interpreter_Client.py
+++ mocks/Mock_Interpreter_Client.py
@@ -2,34 +2,32 @@ from pythonosc.udp_client import SimpleUDPClient
 import time
 import random
 import pickle
+from config import settings
+from song import song_machine
 
-
-mock_client = SimpleUDPClient('127.0.0.1', 5020)
+mock_client = SimpleUDPClient(settings.ip, settings.INTERPRETER_PORT)
 texts = ['Dies ist der nullte Kommentar von Mock_Interpreter_Client',
          'Dies ist der erste Kommentar',
-         'der zweite Kommentar' ,
-         'der dritte Kommentar von Mock_Interpreter_Client. Ich weiss gar nicht mehr, wann ich das letzte Mal so richtig viel geschrieben habe',
+         'der zweite Kommentar',
+         'der dritte Kommentar von Mock_Interpreter_Client. '
+         'Ich weiss gar nicht mehr, wann ich das letzte Mal so richtig viel geschrieben habe',
          'Dies ist der vierte und finite Kommentar von Mock_Interpreter_Client']
-
 level_values = [3, 5, 8]
 
-osc_dict = {}
 
-if __name__ == "__main__":
-
-    # add file to path so import works, see https://stackoverflow.com/a/19190695/7414040
-    from os import sys, path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    from config import settings
-    from song import song_server, song_machine
-
-    song_machine = song_machine.create_instance(settings.song_path)
-    categories = list(song_machine.category_counter.keys())
+def run_mock():
+    machine = song_machine.create_instance(settings.song_path)
+    categories = list(machine.category_counter.keys())
 
     while True:
-        osc_dict = {'text': texts[random.randint(0, 4)],
-                    'cat': categories[random.randint(0, len(categories))],
-                    'level': level_values[random.randint(0, 2)]}
+        osc_dict = {'text': random.sample(texts, 1)[0],
+                    'cat': random.sample(categories, 1)[0],
+                    'level': random.sample(level_values, 1)[0]
+                    }
         osc_map = pickle.dumps(osc_dict)
-        mock_client.send_message(song_server.INTERPRETER_TARGET_ADDRESS, osc_map)
+        mock_client.send_message(settings.INTERPRETER_TARGET_ADDRESS, osc_map)
         time.sleep(2)
+
+
+if __name__ == "__main__":
+    run_mock()
