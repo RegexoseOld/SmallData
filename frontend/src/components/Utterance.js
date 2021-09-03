e88ca4f2058cbf9a2d83d6e4fa6d952970ae0a3c
staude <stoide@googlemail.com>
2021-09-03 21:53:53 +0200

    Ignore sentences that the language model doenst understand

--- frontend/src/components/Utterance.js
+++ frontend/src/components/Utterance.js
@@ -25,7 +25,7 @@ export default class Utterance extends Component {
         if (this.state.text === "") {
             toast.error('Bitte einen Kommentar eingeben')
         } else {
-            const url = "http://192.168.1.164:8000";
+            const url = "http://127.0.0.1:8000";
             fetch(url + "/api/utterances/", {
                 method: "POST",
                 body: JSON.stringify(this.state),
@@ -41,8 +41,13 @@ export default class Utterance extends Component {
     }
 
     notify(data) {
-        toast.info(<div>You said: {data['text']}
+        if (data['category'].name === 'unknown') {
+            toast.info(<div>Die Meinungsorgel funktioniert nur mit richtigen Sätzen. <br />
+                        Bitte versuche es noch einmal!</div>);
+        } else {
+            toast.success(<div>You said: {data['text']}
                     <br /> Machine thinks: {data['category']['name']} </div>);
+        }
     }
 
     handleChange(event) {
--- webserver/smalldata/views.py
+++ webserver/smalldata/views.py
@@ -18,7 +18,6 @@ from webserver.sound.UDPClient import MusicClient
 from config import settings
 
 
-
 clf = Classifier(settings.DATA_DIR)
 #   Client for a simple Feedback from Ableton Live
 song_client = MusicClient(settings.ips['song_server'], settings.SONG_SERVER_PORT)
@@ -51,20 +50,18 @@ class UtteranceView(viewsets.ModelViewSet):
         # lookup found category in database
         #  TODO: build a test during startup to make sure the db and the model reproduce the same categories!
         categories = Category.objects.all().filter(name=str(cat))
-        if categories:
-            category = categories[0]
-        else:  # if db is inconsistent with model, use a random cat
-            categories = Category.objects.all()
-            category = categories[random.randint(0, len(categories)-1)]
-            print('WARNING: no matching category in db! Using random assignment')
+        if not categories:
+            print('WARNING: category {} not in db! Fix your db setup!'.format(cat))
 
-        serializer.validated_data["category"] = category
+        category = categories[0]
 
         #  save result in db
+        serializer.validated_data["category"] = category
         super(UtteranceView, self).perform_create(serializer)
         print('cat: {}\ntext {}'.format(category.name, text))
 
-        send_to_music_server(text.encode("utf-8"), category.name)
+        if cat[0] != clf.UNCLASSIFIABLE:
+            send_to_music_server(text.encode("utf-8"), category.name)
 
 
 class CategoryView(viewsets.ModelViewSet):
