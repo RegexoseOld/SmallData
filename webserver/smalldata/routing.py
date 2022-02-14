6ee7360e52ce0313b26da9ca0f795c8dbefe6862
staude <stoide@googlemail.com>
2022-02-14 11:57:52 +0100

    Delete Webserver & Song
     - keep only SuperCollider and Processing apps

--- webserver/smalldata/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from webserver.smalldata import consumers
-
-websocket_urlpatterns = [
-    # We use re_path() due to limitations in URLRouter.
-    re_path(r"ws/utterance", consumers.UtteranceConsumer.as_asgi()),
-]
