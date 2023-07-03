122ade50340cc1245f625e64c0df663cab108619
Benjamin Staude <stoide@googlemail.com>
2023-07-03 13:23:52 +0200

    Cleanup logic

--- utilities/play_csv.py
+++ utilities/play_csv.py
@@ -14,32 +14,30 @@ def send_data(index, time):
     row = df.iloc[index]
     text = row['Utterance']
     category = row['Category']
-    print("sending", text, category)
+    print("sending", text, '-', category)
     collider_client.send_message(COLLIDER_ROUTE, [text, category])
+    set_timer(index+1, time)
 
-    index += 1
+
+def set_timer(index, prev_time):
     if index < len(df):
-        set_timer(time, index)
+        row = df.iloc[index]
+        time = datetime.datetime.strptime(row['Time'], '%H:%M:%S:%f')
+        interval = time-prev_time
+        t = threading.Timer(interval.total_seconds(), send_data, args=(index, time))
+        t.start()
     else:
         print("done")
-
-
-def set_timer(prev_time, index):
-    row = df.iloc[index]
-    time = datetime.datetime.strptime(row['Time'], '%H:%M:%S:%f')
-    interval = time-prev_time
-    t = threading.Timer(interval.total_seconds(), send_data, args=(index, time))
-    t.start()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(
         prog='PlayCsv',
         description='plays the content of a csv as if it was a live show (send to supercollider)')
-
     parser.add_argument('filename')
     args = parser.parse_args()
+    
     df = pd.read_csv(args.filename)
     first_row = df.iloc[0]
     first_time = datetime.datetime.strptime(first_row['Time'], '%H:%M:%S:%f')
-    set_timer(first_time, 0)
+    set_timer(0, first_time)
